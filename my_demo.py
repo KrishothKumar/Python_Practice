a6586770accd55b996a68ad89113bf370457b86a
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-06 20:11:36 +0530

    My intial commit for python practice

---
+++ my_demo.py
@@ -0,0 +1,20 @@
+# # !/bin/python3
+# print("Hello, World!")
+# print("Python is a wonderful language")
+# y= "I am beginner in Python"
+# print(y)
+# print(y[1])
+# print(y[2:5])
+# x = 1
+# s = 2.8
+# z = 1j
+# print(type(x))
+# print(type(y))
+# print(type(s))
+# print(type(z))
+# a = int(2.8)
+# b = float(1)
+# print(a)
+# print(b)
+wel = input("Enter Your Name: ")
+print("Hi " + wel)
