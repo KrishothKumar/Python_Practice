ac5603a8e7c1751480cd20fdfe731bf0c8fb877d
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-15 22:05:24 +0530

    Code Has Changed to find prime or not Befor Commit was Finding odd or Even

--- number_type.py
+++ number_type.py
@@ -1,17 +1,22 @@
 # This program is to find whether give input is prime or not
 
 def prime(num):
-    if not (num%2 == 0):
-        return 1
-    else:
-        return 0
+    for i in range(2, num):
+        if(num % i == 0):
+            return 1
+        else:
+            return 0
+    # if not (num%2 == 0):
+    #     return 1
+    # else:
+    #     return 0
 
 if __name__ == "__main__":
     # nu is reference of input Number and i is reference of prime function input
     nu=int(input("Enter the Number: "))
     i = prime(nu)
 
-    if i == 1:
+    if i == 0:
         print("Giver Value is Prime")
     else:
         print("Given Value is Not a Prime")
