e859b2805f3797f00290aeb6aa17b2e0b1fb2c2e
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-15 21:32:21 +0530

    Code Has Changed to find prime is or not

--- number_type.py
+++ number_type.py
@@ -1,10 +1,17 @@
 # This program is to find whether give input is prime or not
 
 def prime(num):
-    if not(num%2 == 0):
-        print("Prime",num)
+    if not (num%2 == 0):
+        return 1
     else:
-        print("Even",num)
+        return 0
 
 if __name__ == "__main__":
-    print(prime(4))
+    # nu is reference of input Number and i is reference of prime function input
+    nu=int(input("Enter the Number: "))
+    i = prime(nu)
+
+    if i == 1:
+        print("Giver Value is Prime")
+    else:
+        print("Given Value is Not a Prime")
