4c7e3cf4eb2c6f34782f88264bdbde7b61794c4f
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-15 17:04:09 +0530

    Changed Code for  Time

--- my_demo.py
+++ my_demo.py
@@ -1,24 +1,14 @@
-# # !/bin/python3
-# print("Hello, World!")
-# print("Python is a wonderful language")
-# y= "I am beginner in Python"
-# print(y)
-# print(y[1])
-<<<<<<< HEAD
-# print(y[2:5])
-=======
-print(y[2:5])
->>>>>>> c122a96d61bf951cf3c9a02cb9c258ad4b7cf061
-# x = 1
-# s = 2.8
-# z = 1j
-print(type(x))
-# print(type(y))
-# print(type(s))
-# print(type(z))
-# a = int(2.8)
-# b = float(1)
-print(a)
-print(b)
-wel = raw_input("Enter Your Name: ")
-print("Hi " + wel)
+import datetime
+
+
+if __name__ == "__main__":
+    def getnam()
+        nam= input("")
+    def getval():
+        t = datetime.datetime.now()
+        hr= t.hour
+        return hr
+    if getval<12:
+        print("Good Morning ")
+    else:
+        print("Good Afternoon ")
