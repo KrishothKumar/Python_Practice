d6afbf0ccf10088d909e9ba03afd4973e620544b
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-15 17:46:51 +0530

    Final Change

--- my_demo.py
+++ my_demo.py
@@ -1,14 +1,21 @@
 import datetime
 
+def get_val():
+    t = datetime.datetime.now()
+    hr = t.hour
+    return hr
+
+def get_nam():
+    nam = input("Enter the Name: ")
+    return nam
 
 if __name__ == "__main__":
-    def getnam()
-        nam= input("")
-    def getval():
-        t = datetime.datetime.now()
-        hr= t.hour
-        return hr
-    if getval<12:
-        print("Good Morning ")
+
+    if get_val()<12:
+        print("Good Morning "+ get_nam())
+    elif get_val()<15:
+        print("Good Afternoon " + get_nam())
+    elif get_val()<19:
+        print("Good Evening " + get_nam())
     else:
-        print("Good Afternoon ")
+        print("Good Night " + get_nam())
