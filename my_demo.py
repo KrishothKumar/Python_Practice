de5c85ae13a4d3a89878f4468cea3d1da34ca8d7
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-15 20:15:57 +0530

    Modified in a input area

--- my_demo.py
+++ my_demo.py
@@ -9,18 +9,18 @@ def get_nam(nam):
     return nam
 
 def display():
+    nams = input("Enter the Name: ")
+    Name=get_nam(nams)
     if get_val()<12:
-        print("Good Morning "+ get_nam())
+        print("Good Morning "+ Name)
     elif get_val()<15:
-        print("Good Afternoon " + get_nam())
+        print("Good Afternoon " + Name)
     elif get_val()<19:
-        print("Good Evening " + get_nam())
+        print("Good Evening " + Name)
     else:
-        print("Good Night " + get_nam())
+        print("Good Night " + Name)
 
 
 if __name__ == "__main__":
 
-    nams = input("Enter the Name: ")
-    print(get_nam(nams))
-    get_val()
+    display()
