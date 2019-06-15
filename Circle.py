31de055e81d8c147c7c2736d6236d741fa6ea9a8
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-15 19:27:40 +0530

    Modified Square and worked with indentation

--- Circle.py
+++ Circle.py
@@ -2,24 +2,21 @@
 
 PI =  3.14
 
+#r*r is repesents squire and PI =3.14
 def area(r):
-    ar = PI * r * r
-    #r*r is repesents squire and PI =3.14
+    ar = PI * (r**2)
     return ar
 
 
+# 2 multiply PI=3.14 multiply r
 def cir(r):
-    cir =  PI * 2 * r
-    # 2 multiply PI=3.14 multiply r
+    cir =  PI * (2 * r)
     return cir
 
 
-# Main function
-
+# Main
 if __name__ == "__main__":
-
     ra = float(input("Enter the Radius of Circle: "))
 
     print("Circumference of Circle is ", cir(ra))
-
     print("Area of Circle is ", area(ra))
