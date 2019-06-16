98719c3b3cff57d47cf4d128365f1000be29340e
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-16 13:02:47 +0530

    Final Code for Printing Previous Prime Number

---
+++ previous_prime.py
@@ -0,0 +1,25 @@
+
+
+def previous_prime(num):
+    for i in range( num-1 , 1 , -1 ):
+        if prime(i):
+            return i
+
+def prime(num):
+    if num == 2:
+        return True
+    elif num%2==0:
+        return False
+
+    for i in range(3, (num//3),2):
+        if(num % i == 0):
+            return False
+
+    return True
+
+#mani
+if __name__ == "__main__":
+    num= int(input("Enter the Number: "))
+
+    i = previous_prime(num)
+    print("If givien Number is",num," and the previous Prime Number is",i)
