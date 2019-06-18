b3a9478f3dd605ce09f949f79d9acf83557c53a5
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-18 19:09:48 +0530

    Issues to be solved

--- text_read.py
+++ text_read.py
@@ -1,10 +1,14 @@
 # This program is used to Read file and displays count of word,sendence and character
 
 def checking_line():
+    file = []
     file = open("file.txt","r")
     count = 0
+    print(type(file))
     for line in file.readlines():
         count = count+1
+        print(type(line))
+    print(type(line))
     file.close()
     return count
 
