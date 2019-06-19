78f8f0fb3d56b8e91a110a8471b2677da93b7f5f
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-19 13:08:58 +0530

    Checking The given String is palindrome or not

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+# This program is used to find the give string is palindrome or not
+
+def palindrome(str):
+    return str == str[::-1]
+
+# Main
+if __name__ == "__main__":
+    x=palindrome("mom")
+    if x:
+        print("is palindrome")
+    else:
+        print("is not palindrome")
