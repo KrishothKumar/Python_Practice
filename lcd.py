f44f5077996bbdffe5fc40f075dbac5d3316c41e
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-19 12:17:24 +0530

    Finding the LCM of two numbers

---
+++ lcd.py
@@ -0,0 +1,14 @@
+# This program is used to display LCD of two numbers
+
+import gcd
+
+def lcd(num,num1):
+    gcd_val = gcd.gcd(num,num1)
+    given_mul = num*num1
+    lcd_val = (given_mul//gcd_val)
+    return lcd_val
+
+
+# Main
+if __name__=="__main__":
+    print(lcd(8,16))
