5b23435ca558fba08e355459b8a0645fa2f356a0
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-19 15:08:35 +0530

    Call previoushello_world.py program and printing string in this program

---
+++ previous_string.py
@@ -0,0 +1,9 @@
+# This is a program is use the Hello_world program and print is new valuse
+
+# Import hello_world program in this program
+from hello_world import word_string
+
+if __name__ == "__main__":
+
+    a= word_string("Krishoth")
+    print(a)
