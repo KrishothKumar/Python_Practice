c611f0ce7918e07c1d37f56c4ff358d76efa78bc
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-19 14:58:13 +0530

    Hello world Program

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# This program is to print String
+
+def word_string(str):
+    return str
+
+
+# Main
+if __name__ == "__main__":
+
+    print(word_string("Hello World"))
+    print(word_string("Python is a wonderful language"))
+    print(word_string("I am beginner in Python"))
