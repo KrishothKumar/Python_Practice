3e77dcc575d2967ab7140aa2d8cb67dd8aeb6a51
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-21 13:50:40 +0530

    Read a text file and return number of sentences, words and character in the file

--- text_read.py
+++ text_read.py
@@ -1,18 +1,27 @@
 # This program is used to Read file and displays count of word,sendence and character
 
-def checking_line():
-    file = []
-    file = open("file.txt","r")
+def checking_line(file):
+    file = open(file,"r")
     count = 0
-    print(type(file))
+    character_count = 0
+    word_count = 0
+
     for line in file.readlines():
+
+        line=line.split()
+        word_count += len(line)
+        line_string = "".join(line)
+        character_count += len(line_string)
         count = count+1
-        print(type(line))
-    print(type(line))
+
     file.close()
-    return count
+    return count, word_count, character_count
 
 
 # Main
 if __name__=="__main__":
-    print(checking_line())
+    file=input("Enter the file name with extention:")
+    line, word, character = checking_line(file)
+    print("Number of lines in given file is",line)
+    print("Number of words in given file is",word)
+    print("Number of character in given file is",character)
