efaf6a2c4b2836aeb3487728d33ddb385417d938
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-22 11:40:35 +0530

    Modified program for word_reverse

--- word_reverse.py
+++ word_reverse.py
@@ -1,12 +1,12 @@
 # This program is used to reverse the given word
 
 def string_reverse(list_string):
-    list_string=list_string.split()
-    list_string.reverse()
+    list_string = list_string.split()
+    list_string = list_string[::-1]
     return " ".join(list_string)
 
 
 # Main
 if __name__ == "__main__":
-    string_list="Pradeep loves swetha but she don't knows it"
+    string_list = "Pradeep loves swetha but she don't knows it"
     print(string_reverse(string_list))
