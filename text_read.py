2a35173cd104b134c2657b0323685f42667fdfc2
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-22 11:40:23 +0530

    Modified progam text read

--- text_read.py
+++ text_read.py
@@ -8,20 +8,20 @@ def checking_line(file):
 
     for line in file.readlines():
 
-        line=line.split()
+        line = line.split()
         word_count += len(line)
         line_string = "".join(line)
         character_count += len(line_string)
         count = count+1
 
     file.close()
-    return count, word_count, character_count
+    return [count, word_count, character_count]
 
 
 # Main
 if __name__=="__main__":
     file=input("Enter the file name with extention:")
-    line, word, character = checking_line(file)
-    print("Number of lines in given file is",line)
-    print("Number of words in given file is",word)
-    print("Number of character in given file is",character)
+    file_count = checking_line(file)
+    print("Number of lines in given file is",file_count[-3])
+    print("Number of words in given file is",file_count[-2])
+    print("Number of character in given file is",file_count[-1])
