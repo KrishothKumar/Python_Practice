18774477b8ded21bc2a07c2e0e3540c5f0d3bd12
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-24 14:01:33 +0530

    Change user input variable name and append to get use input

--- list_value.py
+++ list_value.py
@@ -7,15 +7,15 @@
 #     elif list[i]>=list[i+1]:
 #         new.append(list[i+1])
 # print(new)
-def user_list(list, use):
+def user_list(list, user_value):
 
     list.sort()
-    return use in list
+    return user_value in list
 
 # Main Statement
 if __name__ == "__main__":
     list = [10,2,3]
-    user_input = 12
+    user_input = int(input("Enter the value to check in list"))
 
     if user_list(list,user_input):
         print("given value is in list")
