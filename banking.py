2389b3afe7b3d398afd5252c0e934a7c37d328c0
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-25 18:18:52 +0530

    Modified code to display the account balance with various transaction

--- banking.py
+++ banking.py
@@ -1,21 +1,27 @@
 # This program is used diposit and withdrawal and show full account_balance
 
-def transaction(user_input):
-    print(type(user_input))
+def transaction(methodToRun):
     account_balance = 0
+
     while True:
-        if user_input[0] == "d" or user_input[0] == "D":
-            account_balance += int(user_input[1::])
-            return account_balance
-        elif user_input[0] =="w" or user_input[0] == "W":
-            account_balance += int(user_input[1::])
-        elif user_input == "end" or user_input == "END":
+        user_inputs =  methodToRun()
+
+        if user_inputs[0] == "d" or user_inputs[0] == "D":
+            account_balance += int(user_inputs[1::])
+
+        elif user_inputs[0] =="w" or user_inputs[0] == "W":
+            account_balance -= int(user_inputs[1::])
+
+        elif user_inputs == "end" or user_inputs == "END":
             break
 
     return account_balance
 
+def user_input():
+    valus=input("Enter The valuse for deposit D followed by Number \nfor withdrawal W followed by Number for stop enter end:\n")
+    return valus
 
-
+# Main
 if __name__=="__main__":
-    print(transaction(input("Enter The valuse for deposit D followed by Number for withdrawal W followed by Number for stop enter end:"
-         )))
+    value1 = transaction(user_input)
+    print ("Know account balance is", value1)
