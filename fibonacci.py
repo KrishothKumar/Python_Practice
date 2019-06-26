371ea3d6b2bc5a9526b1d0490ee4e87147be80a5
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-26 12:05:36 +0530

    Modifed Code for fibonacci series

--- fibonacci.py
+++ fibonacci.py
@@ -1,27 +1,25 @@
 # This program is to print Nth fibonacci number
 
-def fibon(t0 = 0, t1 = 1):
-    temp= t0+t1
-    t0=t1
-    t1=temp
-    return t0,t1
-
-
-def display(num,t0,t1):
-    i=0
+# This function is used to display the postion of values in fibonacci series
+def display(user_number, t0 = 0, t1 = 1):
+    iteration_stop=1
     list=[0,1]
     while True:
         # print(t0," , ",t1)
-        t0,t1 = fibon(t0,t1)
+        temp= t0+t1
+        t0=t1
+        t1=temp
+        # t0,t1 = fibon(t0,t1)
         list.append(t1)
-        i=i+1
-        if i>num:
+        iteration_stop = iteration_stop+1
+        if iteration_stop > user_number:
             break
     return list
 
 
 # main
 if __name__ == "__main__":
-    num= int(input("Enter the number: "))
-    list=display(num,t0,t1)
-    print("Fibonacci Series of",num,"th position in series is",list[num-1])
+    user_number = int(input("Enter the number: "))
+    list = display(user_number)
+    print(list)
+    print("Fibonacci number of",user_number,"th position in series is",list[user_number-1])
