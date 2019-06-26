ad30783c8732ab4a276e33f961cc36a1078e9a31
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-26 16:06:40 +0530

    Bug is nee to fix

---
+++ tic_tac_toe.py
@@ -0,0 +1,23 @@
+# This program is used to Play TIC TAC TOE
+import array
+
+def play_logic():
+
+
+    i,x= lis[7]
+    print(i,x)
+
+def user_input():
+    valus = int(input("Enter The valuse for deposit D followed by Number \nfor withdrawal W followed by Number for stop enter end:\n"))
+    return valus
+
+# play_logic()
+if __name__ =="__main__":
+    list = [[0,0,0],[0,0,0],[0,0,0]]
+    lis= [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]]
+    print(list[0],end =" ")
+    print("  | 0 | 1 | 2 |")
+    print(list[1],end =" ")
+    print("  | 3 | 4 | 5 |")
+    print(list[2],end =" ")
+    print("  | 6 | 7 | 8 |")
