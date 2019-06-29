d47ad4b29bd575edbfc018038dd0714ad34e7604
KrishothKumar <Krishothkumar@hotmail.com>
2019-06-29 13:33:12 +0530

    Implement program with lower, upper and camelcase

--- string_type.py
+++ string_type.py
@@ -4,7 +4,7 @@ def lowercase(string):
     for i in range(0,len(string)):
         if (ord(string[i])>=65) and (ord(string[i])<=90):
 
-            string =string[:i]+ chr(ord(string[i])+32)+string[i+1:]
+            string = string[:i]+ chr(ord(string[i])+32)+string[i+1:]
 
     return string
 
@@ -12,22 +12,22 @@ def uppercase(string):
     for i in range(0,len(string)):
         if (ord(string[i])>=97) and (ord(string[i])<=122):
 
-            string =string[:i]+ chr(ord(string[i])-32)+string[i+1:]
+            string = string[:i]+ chr(ord(string[i])-32)+string[i+1:]
 
     return string
 
 def camelcase(string):
-    pass
+    for i in range(0,len(string)):
+        if (ord(string[i])== 32):
+            if (ord(string[i+1])>=97) and (ord(string[i+1])<=122):
+                string = string[:i+1]+ chr(ord(string[i+1])-32)+string[i+2:]
 
-
+    return string
 
 # Main statement
 if __name__ == "__main__":
-    str = "KrishZ"
-    print(str[5:])
-    print(chr(0))
-    print(chr(1))
-    print(chr(2))
-    print(chr(3))
-    print(chr(4))
-    print(lowercase(str))
+    string = input("Enter the string :")
+
+    print(lowercase(string))
+    print(uppercase(string))
+    print(camelcase(string))
