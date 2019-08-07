4673ab0eb5f348781ddce5ec3ed936cbf3a47614
KrishothKumar <Krishothkumar@hotmail.com>
2019-08-07 19:42:42 +0530

    Creating Of contact book with json

--- contact_book.py
+++ contact_book.py
@@ -5,9 +5,14 @@ def display():
         array = f.read()
         # array = json.dumps(array)
         datastore = json.loads(array)
+        with open("contacts.json", "wb") as array:
+            array.seek(4, 0)
+            array.write( "Python is a great language.\nYeah its great!!\n");
+
     print("Name          Phone_No          E_mail_ID")
     for i in datastore["contact"]:
         print(i["Name"],"  ",end=" ")
         print(i["Phone_No"],"  ",end=" ")
         print(i["E_mail_ID"])
+
 display()
