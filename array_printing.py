6ec3e462b5f3db446bb2564946c08c86bc707bb5
KrishothKumar <Krishothkumar@hotmail.com>
2019-08-07 19:39:05 +0530

    Print Matrix puzzle

--- array_printing.py
+++ array_printing.py
@@ -1,23 +1,30 @@
 # This program is used to print the valus as per the output
 
 def print_patten(n):
-    arr = [[1, 1, 1, 1, 1],[1, 1, 1, 1, 1],[1, 1, 1, 1, 1],[1, 1, 1, 1, 1],[1, 1, 1, 1, 1]]
+    # arr = [[1, 1, 1, 1, 1],[1, 1, 1, 1, 1],[1, 1, 1, 1, 1],[1, 1, 1, 1, 1],[1, 1, 1, 1, 1]]
+    # b=5
+    # arr = [[0]*b]*b        not array
+    arr = [[0 for col in range(5)]for row in range(5)]
+
+    # print(arr)
     for i in range(5):
         for j in range(5):
-            if i == 0 or j == 0 or i ==4 or j == 4:
+            if i == 0 or i ==4 or j == 0 or j == 4:
                 arr[i][j] = n
             elif i == 1 or i == 3 or j == 1 or j == 3:
                 arr[i][j] = n-1
             elif i == 2:
                 arr[i][j] = n-2
+
     return arr
 
 # Main
 if __name__ == "__main__":
     n= int(input("Enter the number to be print in patten:"))
     arr = print_patten(n)
-    print(arr[0])
-    print(arr[1])
-    print(arr[2])
-    print(arr[3])
-    print(arr[4])
+
+    print(*arr)
+    # print(arr[1])
+    # print(arr[2])
+    # print(arr[3])
+    # print(arr[4])
